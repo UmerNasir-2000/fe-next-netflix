dca8b898190b528522d5a0503b8d40d3efd5de0e
umer-maqsad <122520233+umer-maqsad@users.noreply.github.com>
2023-03-19 13:51:52 +0500

    [FEAT] Added assets :muscle:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,18 @@ import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS theme={{}}>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colors: {
+          primary: ["#E50914", " #B00710"],
+          secondary: ["#303030", " #000000"],
+          white: ["#FFFFFF", " #F5F5F5"],
+        },
+        fontFamily: "Nunito Sans, sans-serif",
+      }}
+    >
       <Component {...pageProps} />
     </MantineProvider>
   );
